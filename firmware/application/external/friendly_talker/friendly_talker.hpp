395c6fc3e3866aac5d71b8f5f0bfa43fd72cd718
winson24 <87272878+winson24@users.noreply.github.com>
2026-02-25 13:52:08 +0800

    Update friendly_talker.hpp

--- firmware/application/external/friendly_talker/friendly_talker.hpp
+++ firmware/application/external/friendly_talker/friendly_talker.hpp
@@ -5,16 +5,20 @@
 
 namespace friendly_talker {
 
+// Function declarations
 void init();
 void set_tx_mode();
 void set_rx_mode();
 void set_volume(uint8_t level);
 void set_frequencies(uint64_t tx, uint64_t rx);
 
+// Global state variables (defined in .cpp file)
 extern bool tx_active;
 extern bool rx_active;
+extern uint8_t volume;
+extern uint64_t tx_freq;
+extern uint64_t rx_freq;
 
-} // namespace
+} // namespace friendly_talker
 
 #endif
-
