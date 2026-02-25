373fac70a7291f608dac8abfccc113db9aeca721
winson24 <87272878+winson24@users.noreply.github.com>
2026-02-25 11:28:34 +0800

    Update and rename walkie_talkie_app.cpp to walkie_talkie.cpp

---
+++ firmware/application/external/friendly_talker/walkie_talkie.cpp
@@ -0,0 +1,53 @@
+#include "walkie_talkie.hpp"
+#include "audio.hpp"
+
+namespace walkie_talkie {
+
+bool tx_active = false;
+bool rx_active = true;
+uint8_t volume = 5;
+uint64_t tx_freq = 446006250;  // PMR channel 1
+uint64_t rx_freq = 446006250;
+
+void init() {
+    audio::init();
+    set_rx_mode();
+}
+
+void set_tx_mode() {
+    if (rx_active) {
+        audio::stop();
+        rx_active = false;
+    }
+    if (!tx_active) {
+        // Set to transmit frequency
+        portapack::set_frequency(tx_freq);
+        audio::start_tx();
+        tx_active = true;
+    }
+}
+
+void set_rx_mode() {
+    if (tx_active) {
+        audio::stop_tx();
+        tx_active = false;
+    }
+    if (!rx_active) {
+        // Set to receive frequency
+        portapack::set_frequency(rx_freq);
+        audio::start_rx();
+        rx_active = true;
+    }
+}
+
+void set_volume(uint8_t level) {
+    volume = level;
+    audio::set_volume(volume * 10);
+}
+
+void set_frequencies(uint64_t tx, uint64_t rx) {
+    tx_freq = tx;
+    rx_freq = rx;
+}
+
+} // namespace
