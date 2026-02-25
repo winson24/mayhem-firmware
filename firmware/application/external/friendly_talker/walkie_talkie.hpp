a93db9a0397370fa6686d5d8b15d3bac70df4468
winson24 <87272878+winson24@users.noreply.github.com>
2026-02-25 11:28:59 +0800

    Add walkie_talkie.hpp header file

---
+++ firmware/application/external/friendly_talker/walkie_talkie.hpp
@@ -0,0 +1,19 @@
+#ifndef WALKIE_TALKIE_HPP
+#define WALKIE_TALKIE_HPP
+
+#include <cstdint>
+
+namespace walkie_talkie {
+
+void init();
+void set_tx_mode();
+void set_rx_mode();
+void set_volume(uint8_t level);
+void set_frequencies(uint64_t tx, uint64_t rx);
+
+extern bool tx_active;
+extern bool rx_active;
+
+} // namespace
+
+#endif
